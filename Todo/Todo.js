142524529a0187413d098af588734ddaee7d5253
daystaraliya <114356339+daystaraliya@users.noreply.github.com>
2025-03-23 07:27:10 +0530

    Add files via upload

---
+++ Todo/Todo.js
@@ -0,0 +1,47 @@
+let todoList = [
+     {
+        items :'buy milk' ,
+        dueDate : '22/03/2025',
+     },
+     {
+        items :'college' ,
+        dueDate :'22/03/2025',
+    },
+];
+
+displayItems();
+
+function addTodo() {
+    let inputElement = document.querySelector('#todo-input');
+     let dateElement = document.querySelector('#todo-date');
+    let todoItem = inputElement.value;
+    let todoDate = dateElement.value;
+    todoList.push({items: todoItem , dueDate: todoDate})
+    inputElement.value = '';
+    dateElement.value = '';
+
+    displayItems();
+    
+}
+
+function displayItems() {
+    let containerElement = document.querySelector('.todo-container');
+    
+    let newHtml = '';
+
+    for(let i=0 ; i<todoList.length ; i++) {
+        let {items ,dueDate} = todoList[i];
+
+        newHtml += `
+            
+            <span>${items}</span>
+            <span>${dueDate}</span>
+            <button class="delete-btn" onClick="todoList.splice(${i} , 1);
+                    displayItems();
+            ">Delete</button>
+            
+        `;
+       
+    }
+    containerElement.innerHTML = newHtml;
+}
